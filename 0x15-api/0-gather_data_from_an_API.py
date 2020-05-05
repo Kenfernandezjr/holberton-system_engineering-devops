6271ec48ccc63fa927063aa6feab2fd3742e78eb
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-05-05 02:24:09 +0000

    Upload test #2, fixed indentation error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,26 +5,25 @@ import requests
 from sys import argv
 
 
-def return_todolist(employee_id):
-    """ function to print todo list """
+if __name__ == "__main__":
+
     EMPLOYEE_NAME = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(
-            employee_id)).json().get('name')
+            'https://jsonplaceholder.typicode.com/users/{}'
+            .format(argv[1])).json().get('name')
 
     NUMBER_OF_DONE_TASKS = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}&&completed=true'
-        .format(employee_id)).json()
+            'https://jsonplaceholder.typicode.com/'
+            'todos?userId={}&&completed=true'
+            .format(argv[1])).json()
 
     TOTAL_NUMBER_OF_TASKS = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'
-        .format(employee_id)).json()
+            'https://jsonplaceholder.typicode.com/todos?userId={}'
+            .format(argv[1])).json()
 
     print("Employee {} is done with tasks({}/{}):".format(
-        EMPLOYEE_NAME, len(NUMBER_OF_DONE_TASKS), len(TOTAL_NUMBER_OF_TASKS)))
+            EMPLOYEE_NAME,
+            len(NUMBER_OF_DONE_TASKS),
+            len(TOTAL_NUMBER_OF_TASKS)))
 
     for lists in NUMBER_OF_DONE_TASKS:
         print('\t{}'.format(lists.get('title')))
-
-
-if __name__ == "__main__":
-    return_todolist(int(argv[1]))
