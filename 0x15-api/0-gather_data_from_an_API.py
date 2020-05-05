5693ea2bb06040275e00fe9cb0b57b4ac937e9a2
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-05-05 01:58:08 +0000

    Upload test #2, changed the position of the imports

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ Api to request certain information """
 
-from sys import argv
 import requests
+from sys import argv
 
 
 def return_todolist(employee_id):
     """ function to print todo list """
     EMPLOYEE_NAME = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}'.format(
-            employee_id)).json().get("name")
+            employee_id)).json().get('name')
 
     NUMBER_OF_DONE_TASKS = requests.get(
         'https://jsonplaceholder.typicode.com/todos?userId={}&&completed=true'
@@ -23,7 +23,7 @@ def return_todolist(employee_id):
         EMPLOYEE_NAME, len(NUMBER_OF_DONE_TASKS), len(TOTAL_NUMBER_OF_TASKS)))
 
     for lists in NUMBER_OF_DONE_TASKS:
-        print('\t {}'.format(lists.get('title')))
+        print('\t{}'.format(lists.get('title')))
 
 
 if __name__ == "__main__":
