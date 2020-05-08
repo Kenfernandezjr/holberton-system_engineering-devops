9c57a66f7426cabc540b551e3fe617a4bcd7ea29
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-05-08 03:39:29 +0000

    Upload test #1, to get the top post of any subreddit submitted

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" python requesting subscribers from given reddit """
+
+import requests
+
+
+def top_ten(subreddit):
+    url = 'https://reddit.com/r/{}/top/.json?limit=10'.format(subreddit)
+    headers = {'user-agent': 'Taskrabbitpower'}
+    response = requests.get(url, headers=headers)
+
+    try:
+        for index in response.json()['data']['children']:
+            print(index['data']['title'])
+    except Exception:
+        return None
