03cd94dfeabde17e58bf34b55602b5d48a7b99cb
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-06-23 15:28:11 +0000

    Gather data from Api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
 """ Api to request certain information """
-
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
+    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId='
+    user_info_url = 'https://jsonplaceholder.typicode.com/users/'
 
-    EMPLOYEE_NAME = requests.get(
-            'https://jsonplaceholder.typicode.com/users/{}'
-            .format(argv[1])).json().get('name')
+    if argv[1]:
+        user_id = argv[1]
+        if int(user_id) > 10 or int(user_id) < 1:
+            exit(0)
 
-    NUMBER_OF_DONE_TASKS = requests.get(
-            'https://jsonplaceholder.typicode.com/'
-            'todos?userId={}&&completed=true'
-            .format(argv[1])).json()
+    user_response = requests.get(user_info_url + user_id)
+    todos_response = requests.get(todo_url + user_id)
 
-    TOTAL_NUMBER_OF_TASKS = requests.get(
-            'https://jsonplaceholder.typicode.com/todos?userId={}'
-            .format(argv[1])).json()
+    user_data = user_response.json()
+    user_todos = todos_response.json()
 
-    print("Employee {} is done with tasks({}/{}):".format(
-            EMPLOYEE_NAME,
-            len(NUMBER_OF_DONE_TASKS),
-            len(TOTAL_NUMBER_OF_TASKS)))
+    completed_todos = []
+    for todo in user_todos:
+        if todo['completed'] is True:
+            completed_todos.append(todo['title'])
 
-    for lists in NUMBER_OF_DONE_TASKS:
-        print('\t {}'.format(lists.get('title')))
+    print("Employee {} is done with tasks({}/{}):".format(user_data['name'],
+                                                          len(completed_todos),
+                                                          len(user_todos)
+                                                          ))
+    for todo in completed_todos:
+        print("\t {}".format(todo))
